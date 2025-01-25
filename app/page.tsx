2f0d5a0f7f9001348d61b00a88762a0c06a1e224
1088orbital <necromantiumcriptoactivo@gmail.com>
2025-01-25 01:35:26 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
-import { PayBlock } from "@/components/Pay";
-import { SignIn } from "@/components/SignIn";
-import { VerifyBlock } from "@/components/Verify";
+import { PayBlock } from "@/components/Pay"
+import { SignIn } from "@/components/SignIn"
+import { VerifyBlock } from "@/components/Verify"
+import { Navbar } from "@/components/Navbar"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-y-3">
-      <SignIn />
-      <VerifyBlock />
-      <PayBlock />
-    </main>
-  );
+    <>
+      <Navbar />
+      <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-y-3 pt-20">
+        <SignIn />
+        <VerifyBlock />
+        <PayBlock />
+      </main>
+    </>
+  )
 }
